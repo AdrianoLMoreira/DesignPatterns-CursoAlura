42f8ae493f8c047ad9e5f8140f08cdb940a82d24
AdrianoLMoreira <adriano.am88.am@gmail.com>
2021-06-23 19:42:02 -0300

    Criação projeto exemplo template method

---
+++ DesignPatterns/src/patternTemplateMethod/relatorios/GeradorDeRelatorios.java
@@ -0,0 +1,30 @@
+package patternTemplateMethod.relatorios;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import patternTemplateMethod.model.Produto;
+
+public abstract class GeradorDeRelatorios {
+
+	public final void gerarRelatorio(List<Produto> listaProduto) {
+		String cabecalho = this.gerarCabecalho();
+		String conteudo = this.gerarConteudo(listaProduto);
+		this.gerarVisualizacao(cabecalho, conteudo);
+	}
+	
+	protected abstract void gerarVisualizacao(String cabecalho, String conteudo);
+	
+	protected abstract String gerarConteudo(List<Produto> produtos);
+	
+	protected String gerarCabecalho() {
+		SimpleDateFormat sdf = new SimpleDateFormat("d/M/y");
+		return new String(
+				"RELATORIO DE PRODUTOS VENDIDOS\n"+
+				sdf.format(new Date()) +
+				"\nALM Sistemas.\n"
+		);
+	}
+	
+}
